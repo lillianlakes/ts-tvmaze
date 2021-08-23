9cf4816335c87197e24d18361c3afc4364f0d4ae
lillianlakes <lillian.lakes@gmail.com>
2021-08-23 16:15:55 -0700

    makes AJAX request for search

--- tvmaze.ts
+++ tvmaze.ts
@@ -5,6 +5,8 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const BASE_URL = `https://api.tvmaze.com`;
+
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,26 +15,24 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+interface Show {
+  id: number;
+  name: string;
+  summary: string;
+  image?: string;
+}
+
+async function getShowsByTerm(term: string): Promise<Show[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const url = `${BASE_URL}/search/shows?q=${term}`;
+  const res = await axios.get(url);
+  return res.data.map(r => 
+    ({
+      id: r.show.id, 
+      name: r.show.name, 
+      summary: r.show.summary, 
+      image: r.show.image?.medium ? r.show.image.medium : `https://tinyurl.com/tv-missing`
+    }));
 }
 
 
